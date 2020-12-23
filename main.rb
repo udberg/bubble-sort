d3485918e751ee89db149a8881e137a0b1ddc701
John, Uduak Peter <johnuduak066@gmail.com>
2020-12-23 14:39:25 +0100

    Update parameter names to fix linter errors

--- main.rb
+++ main.rb
@@ -1,17 +1,16 @@
-
 def bubble_sort(arr)
-  bubble_loop(arr) do |x, i|
-    swap_places(arr, i) if arr[i + 1] && x > arr[i + 1]
+  bubble_loop(arr) do |x, ind|
+    swap_places(arr, i) if arr[i + 1] && x > arr[ind + 1]
   end
 
   arr
 end
 
 def bubble_sort_by(arr)
-  bubble_loop(arr) do |x, i|
+  bubble_loop(arr) do |_x, ind|
     if arr[i + 1]
-      count = yield(arr[i], arr[i + 1])
-      swap_places(arr, i) if count.positive?
+      count = yield(arr[ind], arr[ind + 1])
+      swap_places(arr, ind) if count.positive?
     end
   end
 
@@ -20,15 +19,14 @@ end
 
 def bubble_loop(arr)
   arr.length.times do
-    arr.each.with_index { |x, i| yield(x, i) }
+    arr.each.with_index { |x, i| yield(x, ind) }
   end
 end
 
 def swap_places(arr, i)
-  arr[i], arr[i + 1] = arr[i + 1], arr[i]
+  arr[ind], arr[ind + 1] = arr[ind + 1], arr[ind]
 end
 
-p bubble_sort(%w[2  8 0 7 9])
+p bubble_sort(%w[2 8 0 7 9])
 
 p bubble_sort_by(%w[Hey hello amazing hi]) { |left, right| left.length - right.length }
-
